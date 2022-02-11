b6d9bff7a41bb3803e9d13c3c6e05eafdb125877
Qasim223 <41677739+Qasim223@users.noreply.github.com>
2022-02-11 13:18:46 +0000

    Password Generator Added

---
+++ Password Generator V2/Password Generator V2/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnGenerate = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TxtPassword = New System.Windows.Forms.TextBox()
+        Me.TxtPassword2 = New System.Windows.Forms.TextBox()
+        Me.TxtPassword3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnGenerate
+        '
+        Me.BtnGenerate.Location = New System.Drawing.Point(237, 112)
+        Me.BtnGenerate.Name = "BtnGenerate"
+        Me.BtnGenerate.Size = New System.Drawing.Size(75, 23)
+        Me.BtnGenerate.TabIndex = 0
+        Me.BtnGenerate.Text = "Generate"
+        Me.BtnGenerate.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label1.Location = New System.Drawing.Point(223, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(103, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Password Generator"
+        '
+        'TxtPassword
+        '
+        Me.TxtPassword.Location = New System.Drawing.Point(30, 86)
+        Me.TxtPassword.Name = "TxtPassword"
+        Me.TxtPassword.Size = New System.Drawing.Size(157, 20)
+        Me.TxtPassword.TabIndex = 3
+        '
+        'TxtPassword2
+        '
+        Me.TxtPassword2.Location = New System.Drawing.Point(193, 86)
+        Me.TxtPassword2.Name = "TxtPassword2"
+        Me.TxtPassword2.Size = New System.Drawing.Size(157, 20)
+        Me.TxtPassword2.TabIndex = 4
+        '
+        'TxtPassword3
+        '
+        Me.TxtPassword3.Location = New System.Drawing.Point(356, 86)
+        Me.TxtPassword3.Name = "TxtPassword3"
+        Me.TxtPassword3.Size = New System.Drawing.Size(157, 20)
+        Me.TxtPassword3.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(237, 141)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Hello"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DodgerBlue
+        Me.ClientSize = New System.Drawing.Size(556, 273)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TxtPassword3)
+        Me.Controls.Add(Me.TxtPassword2)
+        Me.Controls.Add(Me.TxtPassword)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnGenerate)
+        Me.Name = "Form1"
+        Me.Text = "Password Generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BtnGenerate As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TxtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents TxtPassword2 As TextBox
+    Friend WithEvents TxtPassword3 As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Password Generator V2/Password Generator V2/Form1.vb
@@ -0,0 +1,78 @@
+﻿Public Class Form1
+
+    Private Sub BtnGenerate_Click(sender As System.Object, e As System.EventArgs) Handles BtnGenerate.Click
+
+        Dim sr As New System.IO.StreamReader("Z:\Kasim\Development\Password Generator\Dictionary.txt")
+
+        Dim iMaxlines As Integer = 0
+        Dim curline As Integer = 0
+        Dim Ran As Integer = 0
+        Dim Number As Integer
+        Dim Symbol As Integer = 0
+        Dim SpecialChrs As String = ""
+        Dim CharsList = "?,>,<,@,',:,;,#,~,},{,],[,=,-,+,_,),(,*,&,%,£,!,ô"
+        Dim RandomClass As New Random()
+        Dim UsableChars() As String
+        Dim sWord As String = ""
+        Dim j As Integer
+
+
+        Do Until sr.EndOfStream = True
+            sr.ReadLine()
+            iMaxlines += 1
+        Loop
+        sr.Dispose()
+        sr.Close()
+
+
+        For j = 1 To 3
+            Randomize()
+            Ran = Rnd() * iMaxlines
+
+            Dim sr2 As New System.IO.StreamReader("Z:\Kasim\Development\Password Generator\Dictionary.txt")
+            curline = 0
+            Do Until curline >= Ran
+                sWord = sr2.ReadLine()
+                curline += 1
+            Loop
+            sr2.Close()
+            sr2.Dispose()
+            sr2 = Nothing
+
+            If sWord.Length > 1 Then
+                sWord = sWord.Substring(0, 1).ToUpper() + sWord.Substring(1).ToLower
+
+            End If
+
+            Randomize()
+            Number = CInt(Math.Floor((999 - 100 + 1) * Rnd())) + 100
+
+
+            UsableChars = Split(CharsList, ",")
+            SpecialChrs = ""
+
+            Symbol = RandomClass.Next(0, UBound(UsableChars))
+            SpecialChrs = SpecialChrs + UsableChars(Symbol)
+
+            Select Case j
+                Case 1
+                    TxtPassword.Text = sWord.Substring(0, 3) & Number & sWord.Substring(3, 3) & SpecialChrs
+
+                Case 2
+                    TxtPassword2.Text = sWord.Substring(0, 3) & Number & sWord.Substring(3, 3) & SpecialChrs
+
+                Case 3
+                    TxtPassword3.Text = sWord.Substring(0, 3) & Number & sWord.Substring(3, 3) & SpecialChrs
+
+            End Select
+        Next
+
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim path As String
+        path = System.IO.Path.GetDirectoryName(
+      System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase)
+        MessageBox.Show(path)
+    End Sub
+
+End Class
